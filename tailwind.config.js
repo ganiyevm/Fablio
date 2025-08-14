f319f5757b3f2fbec562d756df8d0a777a54e9d7
ganiyevm <xusniddinganiyev47@gmail.com>
2025-08-14 14:22:50 +0500

    doesn't deploy some images

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@
 export default {
   content: [
     "./index.html",
-    "./src/**/*.{vue,js,ts,jsx,tsx}", // barcha Vue va JS fayllar uchun
+    "./src/*/.{vue,js,ts,jsx,tsx}", // barcha Vue va JS fayllar uchun
   ],
   theme: {
     extend: {},
