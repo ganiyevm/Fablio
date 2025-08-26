729f9d8625c52bc6b636fc3c8cf5ee4cdd42dbd9
ganiyevm <xusniddinganiyev47@gmail.com>
2025-08-26 11:37:02 +0500

    init fronted backend

--- fronted/vite.config.js
+++ fronted/vite.config.js
@@ -1,15 +1,23 @@
 // vite.config.js
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import { resolve } from 'path'
-import { fileURLToPath, URL } from 'node:url'
+// import { defineConfig } from 'vite'
+// import vue from '@vitejs/plugin-vue'
+// import { resolve } from 'path'
+// import { fileURLToPath, URL } from 'node:url'
 
-export default defineConfig({
-  plugins: [vue()],
-  base: '/Fablio/',
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
-    },
-  },
+// export default defineConfig({
+//   plugins: [vue()],
+//   base: '/Fablio/',
+//   resolve: {
+//     alias: {
+//       '@': fileURLToPath(new URL('./src', import.meta.url)),
+//     },
+//   },
+// })
+import { createRouter, createWebHistory } from 'vue-router'
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL), // ✅
+  routes: [/* ... */],
 })
+export default router
+
